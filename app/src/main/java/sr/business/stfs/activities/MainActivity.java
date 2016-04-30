75b8a7ab5d9f2a780dd71185c754bc3ad4238bc4
jurian360 <jurian360@gmail.com>
2016-04-30 00:16:09 -0300

    Added 3 fragments to the drawer

--- app/src/main/java/sr/business/stfs/activities/MainActivity.java
+++ app/src/main/java/sr/business/stfs/activities/MainActivity.java
@@ -2,9 +2,10 @@ package sr.business.stfs.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,8 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import sr.business.stfs.R;
+import sr.business.stfs.fragments.CategoryFragment;
+import sr.business.stfs.fragments.ProductOverviewFragment;
+import sr.business.stfs.fragments.UserFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -87,15 +92,16 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            ft.replace(R.id.main_frame,new ProductOverviewFragment()).commit();
         } else if (id == R.id.nav_gallery) {
-
+            ft.replace(R.id.main_frame,new UserFragment()).commit();
         } else if (id == R.id.nav_slideshow) {
-
+            ft.replace(R.id.main_frame,new CategoryFragment()).commit();
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
